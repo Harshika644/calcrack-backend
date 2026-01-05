6c1783c0fd1b81f093e1ab13f81aedc53b39db3e
Harshika Sharma <harshikas621@gmail.com>
2026-01-05 12:33:39 +0530

    Fix list route crash on Render

--- routes/list.js
+++ routes/list.js
@@ -1,35 +1,25 @@
-const db = require('../db');
-var express = require('express');
-var router = express.Router();
-const dotenv = require('dotenv');
+const express = require("express");
+const db = require("../db");
 
-dotenv.config();
+const router = express.Router();
 
-// Require controller modules.
-var issue_controller = require('../controllers/issueController');
+/* ======================
+   MULTI FILE UPLOAD (SAFE)
+   ====================== */
+router.post("/", (req, res, next) => {
+  let upload;
 
-// GET list home page.
-router.get('/', issue_controller.index);
+  try {
+    upload = db.upload;
+  } catch (err) {
+    return res.status(503).json({ error: "Upload service not ready" });
+  }
 
-// GET request for creating an ISSUE. NOTE This must come before routes that display ISSUE (uses id).
-router.get('/issue/create', issue_controller.issue_create_get);
-
-// @route GET /issue/filedisplay
-// @desc display file attachment in new page
-router.get('/issue/filedisplay/:id', issue_controller.file_display);
-
-// @route PUT /issue/create
-// @desc upload multiple files
-router.post(
-  '/issue/create',
-  db.upload.array('file', 3),
-  issue_controller.upload_files
-);
-
-// GET request for list of all Issue items.
-router.get('/issues', issue_controller.issue_list);
-
-// GET request for one Issue.
-router.get('/issue/:id', issue_controller.issue_detail);
+  upload.array("file", 3)(req, res, (err) => {
+    if (err) return res.status(500).json(err);
+    res.json({ message: "Files uploaded successfully" });
+  });
+});
 
 module.exports = router;
+
